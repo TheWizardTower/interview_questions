5bf800538554207dfcbd51f42e1643a6cbdae661
Adam McCullough <merlinfmct87@gmail.com>
2020-02-22 12:52:40 -0800

    Add a type signature.
    I'm pedantic and fussy, and type signatures _everywhere_ help me keep track of
    what's going on in my code.

--- concordance/haskell/app/Main.hs
+++ concordance/haskell/app/Main.hs
@@ -1,23 +1,25 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Main where
 
-import Control.Monad (mapM_)
-import qualified Data.ByteString as B
-import qualified Data.List as L
-import qualified Data.Map as M
-import qualified Data.Set as S
-import Data.Text (Text)
-import qualified Data.Text as T
+import           Control.Arrow      (second)
+import           Control.Monad      (mapM_)
+import qualified Data.ByteString    as B
+import qualified Data.List          as L
+import qualified Data.Map           as M
+import qualified Data.Set           as S
+import qualified Data.Text          as T
 import qualified Data.Text.Encoding as TE
-import Text.Printf (printf)
-import Control.Arrow (second)
+import           Prelude            (IO, Int, Ord, map, take, ($), (+), (.))
+import           Text.Printf        (printf)
 
 -- Thanks Yorgey
 transMap :: (Ord k, Ord a) => M.Map k a -> M.Map a (S.Set k)
 transMap = M.fromListWith S.union . map (second S.singleton . swap) . M.toList
   where swap (one, two) = (two, one)
 
+
+countWords :: [T.Text] -> M.Map T.Text Int
 countWords wordList =
   L.foldl' f M.empty wordList
   where f wordMap word =
