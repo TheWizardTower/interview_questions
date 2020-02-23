d4d3d904e590a096dddee97bd19801f0d470dcd3
Adam McCullough <merlinfmct87@gmail.com>
2020-02-22 18:12:52 -0800

    Added command line arguments, which enabled a few other features.
    Specifically:
    
    1. We can read from files. If no file is specified, fall back on stdin.
    2. We can set how many words we print. If the file has fewer unique words than
    we ask for, silently print the whole list.

--- concordance/haskell/app/Main.hs
+++ concordance/haskell/app/Main.hs
@@ -2,16 +2,48 @@
 
 module Main where
 
-import           Control.Arrow      (second)
-import           Control.Monad      (mapM_)
-import qualified Data.ByteString    as B
-import qualified Data.List          as L
-import qualified Data.Map           as M
-import qualified Data.Set           as S
-import qualified Data.Text          as T
-import qualified Data.Text.Encoding as TE
-import           Prelude            (IO, Int, Ord, map, take, ($), (+), (.))
-import           Text.Printf        (printf)
+import           Control.Arrow       (second)
+import           Control.Monad       (mapM_)
+import qualified Data.ByteString     as B
+import qualified Data.List           as L
+import qualified Data.Map            as M
+import           Data.Semigroup      ((<>))
+import qualified Data.Set            as S
+import qualified Data.Text           as T
+import qualified Data.Text.Encoding  as TE
+import           Options.Applicative as A
+import           Prelude             (IO, Int, Ord, map, length, take, ($), (>=), (+), (.))
+import           System.IO           (IOMode (..), withFile)
+import           Text.Printf         (printf)
+
+data CommandArguments = CommandArguments
+  { filename :: T.Text
+  , numWords :: Int
+  }
+
+cli_parser :: Parser CommandArguments
+cli_parser = CommandArguments
+  <$> A.strOption
+   ( long "filename"
+  <> short 'f'
+  <> metavar "FILENAME"
+  <> help "Filename to read in as input. If left blank, will read from stdin."
+  <> value (T.pack "")
+  <> showDefault
+   ) <*> A.option A.auto
+   ( long "number-of-words"
+  <> short 'n'
+  <> metavar "NUMBER"
+  <> help "Number of most-used words to print. Defaults to 10."
+  <> value 10
+  <> showDefault
+   )
+
+readFromFileOrStdin :: T.Text -> IO B.ByteString
+readFromFileOrStdin fname =
+  if (T.null fname) then B.getContents
+  else withFile (T.unpack fname) ReadMode (B.hGetContents)
+
 
 -- Thanks Yorgey
 transMap :: (Ord k, Ord a) => M.Map k a -> M.Map a (S.Set k)
@@ -27,18 +59,27 @@ countWords wordList =
         updateWordMap wordMap word =
           M.insertWith (+) word 1 wordMap
 
+takeUpTo :: Int -> [a] ->  [a]
+takeUpTo count list = if (length list) >= count then list else take count list
+
 printWordSummary :: (Int, S.Set T.Text) -> IO ()
 printWordSummary (count, word) = printf "%7d %s\n" count (S.elemAt 0 word)
 
 main :: IO ()
 main = do
-  input <- B.getContents
+  let opts = info (cli_parser <**> helper)
+        ( fullDesc
+        <> progDesc "Build a concordance from an input file, and show the N most frequent words."
+        <> header "Concordance-Haskell -- a concordance builder for input files built in Haskell."
+        )
+  args <- execParser opts
+  input <- readFromFileOrStdin $ filename args
   let text = TE.decodeUtf8 input
       text :: T.Text
       wordList = T.words text
       wordList :: [T.Text]
       wordCounts = countWords wordList
       wordCounts :: M.Map T.Text Int
-      topWords = take 10 $ M.toDescList $ transMap wordCounts
+      topWords = take (numWords args) $ M.toDescList $ transMap wordCounts
       topWords :: [(Int, S.Set T.Text)]
   mapM_ printWordSummary topWords
