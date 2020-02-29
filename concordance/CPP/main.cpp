9cd33549865e076d7edb4b1bc46e81bfe5c2d055
Adam McCullough <merlinfmct87@gmail.com>
2020-02-29 01:06:25 -0800

    Pulling in more libraries and writing the input IO function.

--- concordance/CPP/main.cpp
+++ concordance/CPP/main.cpp
@@ -1,13 +1,43 @@
+#include <fstream>
+#include <sstream>
 #include <iostream>
+#include <string>
 #include <map>
 
+using std::cin;
+using std::cerr;
 using std::cout;
 using std::endl;
+using std::ifstream;
+using std::ostringstream;
 using std::map;
 using std::string;
 
+string getInput(string filename) {
+  if (filename == "") {
+    ostringstream ss;
+    ss << cin.rdbuf();
+    string result = ss.str();
+    return result;
+  }
+  ifstream inFile;
+  inFile.open(filename);
+  if (!inFile) {
+    cerr << "Unable to open filename " << filename << endl;
+    exit(-1);
+  }
+  string buf;
+  string result;
+  while (inFile >> buf) {
+    result += buf;
+  }
+
+  return result;
+}
 
 int main() {
+  string filename = "";
+  string input = getInput(filename);
   map<string, int> wordMap;
   return 0;
 }
