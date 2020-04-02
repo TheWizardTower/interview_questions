0c0a197ee2e629f427072c109922bcd9533f506f
Adam McCullough <merlinfmct87@gmail.com>
2020-04-02 04:18:17 -0700

    Commit some tests; they're all passing nicely now. :)

--- boggle/CPP/main.cpp
+++ boggle/CPP/main.cpp
@@ -23,6 +23,9 @@ list<string> boggleTraversal(list<list<char>> boggleMatrix,
 
 list<string> boggleSolver(list<list<char>> boggleMatrix, Trie dictionary) {
   list<string> result;
+  auto boardSize = boggleMatrix.size();
+  list<list<bool>> visited(boardSize, list<bool>(boardSize, false));
+
   for (auto iter_i = boggleMatrix.begin(); iter_i != boggleMatrix.end();
        ++iter_i) {
     for (auto iter_j = *iter_i->begin(); iter_j != *iter_i->end(); ++iter_j) {
@@ -35,6 +38,17 @@ list<string> boggleSolver(list<list<char>> boggleMatrix, Trie dictionary) {
 int main() {
   list<string> dictionary{"APP", "APPLE", "APPLY", "BUS", "BUSINESS"};
   auto t = Trie::makeTrie(dictionary);
+  cout << traverseTrie(t) << endl;
+
+  cout << "findPrefix 'APPL': " << t.searchPrefix("APPL") << endl;
+  cout << "findPrefix 'BUSIN': " << t.searchPrefix("BUSIN") << endl;
+  cout << "findPrefix 'BUTTTHANDER': " << t.searchPrefix("BUTTTHANDER")
+       << endl; // yes, I've been watching dice, camera, action.
+  cout << "findWord 'APP': " << t.searchWord("APP") << endl;
+  cout << "findWord 'APPL': " << t.searchWord("APPL") << endl;
+  cout << "findWord 'BUSIN': " << t.searchWord("BUSIN") << endl;
+  cout << "findWord 'BUSINESS': " << t.searchWord("BUSINESS") << endl;
+
   cout << traverseTrie(t) << endl;
 
   list<list<char>> boggleGrid{
