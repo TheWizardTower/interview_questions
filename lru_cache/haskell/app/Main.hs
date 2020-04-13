f2d818bb64d1d4088eee50482bb62da9d3b5569d
Adam McCullough <merlinfmct87@gmail.com>
2020-04-13 15:50:50 -0700

    More workout goodness.

--- lru_cache/haskell/app/Main.hs
+++ lru_cache/haskell/app/Main.hs
@@ -12,6 +12,11 @@ main = do
   let myCache = makeSizedLRU 5 :: LRUCache String String
       mapVals = [("Adam", "McCullough"), ("Daggerfall", "Fun Dungeon Torture"), ("Hollow Knight", "Metroidvania"), ("ARMA 3", "Fun Milsim Torture"), ("EUIV", "History Nerd Candy"), ("Adam", "Overwrite-test"), ("Neverwinter Nights", "NERRRRRRDS") ]
       myCache' = foldl' (\cache (key, val) -> insertIntoCache cache key val) myCache mapVals
-  print "I have a cache!"
+      myCache'' = insertIntoCache myCache' "key" "value"
+      myCache'3 = insertIntoCache myCache'' "key1" "value1"
+  print "I have a cache! Initial value:"
   print myCache
+  print "Post list-addition:"
   print myCache'
+  print myCache''
+  print myCache'3
