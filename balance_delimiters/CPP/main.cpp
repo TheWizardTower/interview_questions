ccc2a2b7d5d2f0835f891d7fce86ebd3d010fb00
Adam McCullough <merlinfmct87@gmail.com>
2020-05-13 00:18:42 -0700

    Initial version of this solution. Hammered out in about 35 minutes.

---
+++ balance_delimiters/CPP/main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <map>
+#include <stack>
+#include <vector>
+// str input -> returns true if balanced '()' '('
+// { []}()
+// "("
+
+using std::string;
+using std::stack;
+using std::cout;
+using std::endl;
+using std::map;
+using std::vector;
+
+bool isBalanced(string input) {
+  stack<char> delimiters;
+  map<char, char> openDelim;
+  openDelim[')'] = '(';
+  openDelim[']'] = '[';
+  openDelim['}'] = '{';
+
+  for (auto const& character : input) {
+    switch(character) {
+      case '\'':
+        if (delimiters.size() != 0 && delimiters.top() == '\'') {
+          delimiters.pop();
+          continue;
+        }
+        if (delimiters.size() != 0 && delimiters.top() != '\'') {
+          delimiters.push(character);
+          continue;
+        }
+      case '"':
+        if (delimiters.size() != 0 && delimiters.top() == '"') {
+          delimiters.pop();
+          continue;
+        }
+        if (delimiters.size() != 0 && delimiters.top() != '"') {
+          delimiters.push(character);
+          continue;
+        }
+      case '(':
+      case '[':
+      case '{':
+        if (delimiters.size() != 0 && (delimiters.top() == '\'' || delimiters.top() == '"')) {
+          continue;
+        }
+        delimiters.push(character);
+        break;
+      case ')':
+      case ']':
+      case '}':
+        if (delimiters.size() != 0 && (delimiters.top() == '\'' || delimiters.top() == '"')) {
+          continue;
+        }
+        if (delimiters.size() != 0 && delimiters.top() == openDelim[character]) {
+          delimiters.pop();
+          break;
+        }
+        return false;
+    }
+  }
+
+  if (!delimiters.empty()) {
+    return false;
+  }
+
+  return true;
+}
+
+int main() {
+  cout << "Hello World!\n";
+  vector<string> tests = {"()", "", "(", ")", "([{}])", "()[}", "c++ is good []{(()}", "\"(\"", "(\")))\")", "'((('()"};
+
+  for (const auto& test: tests) {
+      cout << "Testing '" << test << "' " << isBalanced(test) << endl;
+  }
+
+}
