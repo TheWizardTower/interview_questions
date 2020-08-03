b4f5114a545656428db62c45502c405298e221b9
Adam McCullough <merlinfmct87@gmail.com>
2020-08-03 16:46:19 -0700

    Add a test, and reformat a list

--- lru_cache/haskell/app/Main.hs
+++ lru_cache/haskell/app/Main.hs
@@ -9,13 +9,22 @@ import           Prelude       (IO, String, print)
 main :: IO ()
 main = do
   let myCache = makeSizedLRU 5 :: LRUCache String String
-      mapVals = [("Adam", "McCullough"), ("Daggerfall", "Fun Dungeon Torture"), ("Hollow Knight", "Metroidvania"), ("ARMA 3", "Fun Milsim Torture"), ("EUIV", "History Nerd Candy"), ("Adam", "Overwrite-test"), ("Neverwinter Nights", "NERRRRRRDS") ]
+      mapVals = [("Adam", "McCullough")
+                , ("Daggerfall", "Fun Dungeon Torture")
+                , ("Hollow Knight", "Metroidvania")
+                , ("ARMA 3", "Fun Milsim Torture")
+                , ("EUIV", "History Nerd Candy")
+                , ("Adam", "Overwrite-test")
+                , ("Neverwinter Nights", "NERRRRRRDS")
+                ]
       myCache' = foldl' (\cache (key, val) -> insertIntoCache cache key val) myCache mapVals
       myCache'' = insertIntoCache myCache' "key" "value"
       myCache'3 = insertIntoCache myCache'' "key1" "value1"
+      myCache'4 = insertIntoCache myCache'3 "Adam" "Did I get overwritten?"
   print "I have a cache! Initial value:"
   print myCache
   print "Post list-addition:"
   print myCache'
   print myCache''
   print myCache'3
+  print myCache'4
